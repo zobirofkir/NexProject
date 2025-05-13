c98b2a916cc6072060883c4f60b86156c7efb663
csw <zobirofkir19@gmail.com>
2025-05-13 11:34:48 +0100

    add lottie in backghround

--- components/HeroComponent.js
+++ components/HeroComponent.js
@@ -1,7 +1,20 @@
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+
 export default function HeroComponent() {
   return (
-    <section className="h-screen w-full bg-gradient-to-br from-gray-100 to-white dark:from-black dark:to-gray-900 flex items-center justify-center px-6">
-      <div className="text-center max-w-3xl">
+    <section className="relative h-screen w-full  dark:from-black dark:to-gray-900 flex items-center justify-center px-6 overflow-hidden">
+      
+      {/* Lottie Background */}
+      <div className="absolute inset-0 z-0 pointer-events-none opacity-30">
+        <DotLottieReact
+          src="https://lottie.host/3969eb6c-ce9c-407e-baf0-e2445526d695/8ReoPrVYsO.lottie"
+          loop
+          autoplay
+        />
+      </div>
+
+      {/* Foreground Content */}
+      <div className="relative z-10 text-center max-w-3xl">
         <h1 className="text-5xl md:text-6xl font-extrabold tracking-tight text-gray-900 dark:text-white mb-6">
           Welcome to <span className="text-purple-600 dark:text-purple-400">MyShop</span>
         </h1>
